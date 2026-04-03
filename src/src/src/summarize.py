179fef122f61afb83609e7f85a340576fddcaa40
Vasanthaleela  MB <vasanthaleela0207@gmail.com>
2026-04-03 17:37:46 +0530

    Delete src/src/src directory

--- src/src/src/summarize.py
+++
@@ -1,7 +0,0 @@
-from textblob import TextBlob
-
-def summarize_text(text):
-    sentences = text.split('.')
-    if len(sentences) > 3:
-        return sentences[0] + "." + sentences[1] + "." + sentences[2]
-    return text
